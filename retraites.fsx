9e54abdecafe3080712396226430805c8c5b857a
Clément Bouillier <clement.bouillier@gmail.com>
2020-01-13 21:44:53 +0100

    Script adapted for salariés

--- retraites.fsx
+++ retraites.fsx
@@ -19,15 +19,17 @@ let simulationBasedOn computePension percentSalaryIncreaseBetweenStartAndRetirem
 
 let simulationCurrentBasedOnSameSalary = simulationBasedOn CurrentSalaries.calculatePension 0m
 let simulationCurrentBasedOnSameSalaryFixedBirthDate = simulationCurrentBasedOnSameSalary 1980m
+let simulationCurrentBasedOnPercentSalaryIncreaseFixedBirthDate percentIncrease = simulationBasedOn CurrentSalaries.calculatePension percentIncrease 1980m
 
 let simulationCurrentSsiBasedOnSameSalaryFixedBirthDate = simulationBasedOn CurrentSsi.calculatePension 0m 1980m
 let simulationCurrentSsiBasedOnPercentSalaryIncreaseBirthDate percentIncrease = simulationBasedOn CurrentSsi.calculatePension percentIncrease 1980m
 
 let simulationCurrentCipavBasedOnSameSalaryFixedBirthDate = simulationBasedOn CurrentCipav.calculatePension 0m 1980m
-let simulationCurrentCipavBasedOnPercentSalaryIncreaseBirthDate percentIncrease = simulationBasedOn CurrentCipav.calculatePension percentIncrease 1980m
+let simulationCurrentCipavBasedOnPercentSalaryIncreaseFixedBirthDate percentIncrease = simulationBasedOn CurrentCipav.calculatePension percentIncrease 1980m
 
 let simulationReformedSalariesBasedOnSameSalary = simulationBasedOn Reformed.calculatePensionSalaries 0m
 let simulationReformedSalariesBasedOnSameSalaryFixedBirthDate = simulationReformedSalariesBasedOnSameSalary 1980m
+let simulationReformedSalariesBasedOnPercentSalaryIncreaseFixedBirthDate percentIncrease = simulationBasedOn Reformed.calculatePensionSalaries percentIncrease 1980m
 
 let simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate = simulationBasedOn Reformed.calculatePensionNonSalaries 0m 1980m
 let simulationReformedNonSalariesBasedOnPercentSalaryIncreaseFixedBirthDate percentIncrease = simulationBasedOn Reformed.calculatePensionNonSalaries percentIncrease 1980m
@@ -39,48 +41,48 @@ open XPlot.GoogleCharts
 let rateOfReturnSeries = "Taux de rendement (retraite annuelle/cotisations payées) selon les revenus (fixes) et par régime",
     "percent",
     [
-        // "Régime de base", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[0].RateOfReturn)
-        // "Régime AGIRC/ARRCO", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[1].RateOfReturn)
-        // "Régime général actuel", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
-        // "Régime réformé des salariés", simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
-        ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
-        ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
-        ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
+        ("Régime de base", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[0].RateOfReturn)
+        ("Régime AGIRC/ARRCO", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[1].RateOfReturn)
+        ("Régime général actuel", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
+        ("Régime réformé des salariés", ""), simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
+        // ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
+        // ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
+        // ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.GlobalRateOfReturn)
     ]
 
 let cotisationsSeries = "Cotisations selon les revenus sur toute une carrière (revenus fixes) et par régime",
     "#,###€",
     [
-        // "Régime de base", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[0].Cotisations)
-        // "Régime AGIRC/ARRCO", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[1].Cotisations)
-        // "Régime général actuel", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
-        // "Régime réformé des salariés", simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
-        ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
-        ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
-        ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
+        ("Régime de base", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[0].Cotisations)
+        ("Régime AGIRC/ARRCO", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[1].Cotisations)
+        ("Régime général actuel", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
+        ("Régime réformé des salariés", ""), simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
+        // ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
+        // ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
+        // ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalCotisations)
     ]
 
 let monthlyPensionSeries = "Retraite mensuelle nette selon les revenus sur toute une carrière (revenus fixes) et par régime",
     "#,###€",
     [
-        // "Régime de base", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[0].MonthlyNetAmount)
-        // "Régime AGIRC/ARRCO", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[1].MonthlyNetAmount)
-        // "Régime général actuel", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
-        // "Régime réformé des salariés", simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
-        ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
-        ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
-        ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
+        ("Régime de base", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[0].MonthlyNetAmount)
+        ("Régime AGIRC/ARRCO", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.ComposedOf.[1].MonthlyNetAmount)
+        ("Régime général actuel", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
+        ("Régime réformé des salariés", ""), simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
+        // ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
+        // ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
+        // ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.TotalMonthlyNetAmount)
     ]
 
 let replacementRateSeries = "Taux de remplacement selon les revenus sur toute une carrière (revenus fixes) et par régime",
     "percent",
     [
-        // "Taux de remplacement régime général actuel, né en 1961", simulationCurrentBasedOnSameSalary 1961m |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
-        // "Régime général actuel, né en 1980", simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
-        // "Régime général réformé, né en 1980", simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
-        ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
-        ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
-        ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
+        ("Régime général actuel, né en 1961", ""), simulationCurrentBasedOnSameSalary 1961m |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
+        ("Régime général actuel, né en 1980", ""), simulationCurrentBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
+        ("Régime général réformé, né en 1980", ""), simulationReformedSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
+        // ("Régime réformé des non salariés", ""), simulationReformedNonSalariesBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
+        // ("Régime SSI actuel", ""), simulationCurrentSsiBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
+        // ("Régime CIPAV actuel", ""), simulationCurrentCipavBasedOnSameSalaryFixedBirthDate |> Seq.map (fun (x, y) -> x, y.NetReplacementRate)
     ]
 
 let percentIncrease = 200m
